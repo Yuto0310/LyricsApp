3197690ab2daffc033017fca49b7886ae9f518e7
Yuto0310 <110749609+Yuto0310@users.noreply.github.com>
2022-12-17 14:50:18 +0900

    [Update] getSpotifyCurrentTrack.py

--- LyricsApp/getSpotifyCurrentTrack.py
+++ LyricsApp/getSpotifyCurrentTrack.py
@@ -3,3 +3,11 @@ def getInfo():
     nameOfTrack = subprocess.run(['osascript', '-e', 'tell application "Spotify" to get name of current track'], capture_output=True, text=True).stdout
     artistOfTrack = subprocess.run(['osascript', '-e', 'tell application "Spotify" to get artist of current track'], capture_output=True, text=True).stdout
     return (nameOfTrack, artistOfTrack)
+
+def confirmRunning():
+    runningBool = subprocess.run(['osascript', '-e', 'if application "Spotify" is running then return true'], capture_output=True,
+                   text=True).stdout
+    if runningBool == 'true\n':
+        return True
+    else:
+        return False
